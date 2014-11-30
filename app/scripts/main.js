892f56edbd6d6eec5c3a41b344dbbde1beba32f5
Victoria Nicholson <saintvictoria@gmail.com>
2014-11-29 21:42:48 -0500

    started

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('The Iron Yard Rocks');
+(function (){
+  angular.module ('journal',[])
+  .constant('PARSE_HEADERS', {
+    headers: {
+      'X-Parse-Application-Id':'IWbER00hd2pgcj7vTr9JltVyZkxOIFlVlXvJILkw',
+      'X-Parse-REST-API-Key': 'Bf1DLpWdq9Bb4o02q1Vi4j2eBovTBLhvz6mUB3tJ',
+      'Content-Type': 'application/json'
+    }
+  })
+
+
+
+
+
+
+
+
+
+}());
